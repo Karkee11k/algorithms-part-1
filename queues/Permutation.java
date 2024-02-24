df63d5b4b5844b2a433022513c3f71e5115464cf
Karkee11k <149058544+Karkee11k@users.noreply.github.com>
2024-02-24 06:54:34 +0530

    Update Permutation.java

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -8,11 +8,11 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Karthikeyan 
  */
 public class Permutation {
-	public static void main(String[] args) {
-		RandomizedQueue<String> q = new RandomizedQueue<>();
-		int k = Integer.parseInt(args[0]); 
+    public static void main(String[] args) {
+	RandomizedQueue<String> q = new RandomizedQueue<>();
+	int k = Integer.parseInt(args[0]); 
 		
-		while (!StdIn.isEmpty()) q.enqueue(StdIn.readString()); 
-		while (k-- > 0) StdOut.println(q.dequeue());
-	}		
+	while (!StdIn.isEmpty()) q.enqueue(StdIn.readString()); 
+	while (k-- > 0) StdOut.println(q.dequeue());
+    }		
 }
